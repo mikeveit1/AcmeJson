0038bcb66b819f007bf5a051f13e0d881c587d25
Michael Veit <mikeveit1@gmail.com>
2020-11-09 16:03:48 -0500

    commit 2

--- AcmeJson/DataService/DataService.swift
+++ AcmeJson/DataService/DataService.swift
@@ -12,7 +12,7 @@ class DataService {
     static let shared = DataService()
     
     func getData(id: String, completion: (Data) -> Void, errorHandler: (Error) -> Void) {
-        guard let path =  URL(string: "https://launchpadapi.mediafly.com/2/items/\(id)?productId=8049e873d1bf4725bccf362395391810&accessToken=e3464dec016648149c1c4c3ad4888d16&accessType=view") else { return }
+        guard let path =  URL(string: "https://launchpadapi.\(Config.Config.acme).com/2/items/\(id)\(Config.Config.access)") else { return }
         
         let url = path
         
